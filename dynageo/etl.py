ccc871b6b88b2e811cecb080f40616faf9c6c369
Smith, Christopher <smith_christopher@bah.com>
2018-11-19 20:54:40 -0500

    Work on project

--- dynageo/etl.py
+++ dynageo/etl.py
@@ -11,31 +11,30 @@ from sqlalchemy.ext.declarative import declarative_base
 from   sqlalchemy.orm    import sessionmaker
 from   sqlalchemy.sql    import text
 
-Base=declarative_base()
 logging.config.fileConfig('logging.conf',disable_existing_loggers=False)
 logger=logging.getLogger( 'sqlalchemy').setLevel(logging.DEBUG)
-logger=logging.getLogger( 'root')
+logger=logging.getLogger( 'dynageo')
 
-SQLFILE  ='sqlite:///ggs664.sqlite'
-ZIPCODES =""
+SQLFILE  ='sqlite:///data/ggs664.sqlite'
+ZIPCODES ="/home/smitty/proj/dynageo/dynageo/data/"
 LOAD_DATA=True #False #
 EXPORTDAT=True #False #
 
+Base=declarative_base()
 engine   = create_engine(SQLFILE, echo=True)
 session  = sessionmaker(bind=engine)()
 conn     = engine.connect()
-rawconn  = engine.raw_connection()
+#rawconn  = engine.raw_connection()
 
 def load_data():
     '''We ETL data from the shapefile, merging the metadata with the
          shape_points into a SQLite table.
     '''
-    for d in shapefile.Reader(DISTRICTS).shapeRecords():
+    for d in shapefile.Reader(ZIPCODES).shapeRecords():
 
 
 if __name__=='__main__':
     '''Construct the database when invoked directly
     '''
     if LOAD_DATA: load_data()
-    engine = create_engine('sqlite:///ggs650.sqlite', echo=True)
     Base.metadata.create_all(engine)
