942c003e75ec75e403882d8a4c1886d876e4f7b1
Smith, Christopher <smith_christopher@bah.com>
2018-11-20 21:58:32 -0500

    Add click menu

---
+++ dynageo.py
@@ -0,0 +1,39 @@
+import click
+
+
+@click.group()
+def main():
+    """Main interface
+    """
+    pass
+
+@main.command()
+def dbInit():
+    """Initialize the stagging db to hold shapefile data
+    """
+    pass
+
+@main.command()
+def dbLoad():
+    """Read shapefile data into staging db.
+    """
+    pass
+
+
+@main.command()
+def dyInit():
+    """Initialize the dynamodb table
+    """
+    pass
+
+@main.command()
+def dyLoad():
+    """Load dynamodb table from the staging db table.
+    """
+    pass
+
+@main.command()
+def dyQuery():
+    """Run a query against dynamodb
+    """
+    pass
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='dynageo',
+    version='0.1',
+    py_modules=['dynageo'],
+    install_requires=[
+        'Click',
+    ],
+    entry_points='''
+        [console_scripts]
+        dynageo=dynageo:main
+    ''',
+)
